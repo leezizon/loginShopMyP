f14a61ad127696f9afd7c25c89ff31b267c48f8d
leezizon <dlwl2255@naver.com>
2025-03-13 19:22:45 +0900

    cros설정지움

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const nunjucks = require('nunjucks');
-const cors = require('cors');
+// const cors = require('cors');
 
 var indexRouter = require('./routes/index');//로그인/로그아웃/세션관리
 var shopRouter = require('./routes/shop');//상점
@@ -17,21 +17,19 @@ var liveRouter = require('./routes/live');//실시간 채팅
 var app = express();
 
 //다른 도메인에서도 api를 쓸수있도록 앱에 cors설정
-// const allowedOrigins = ['https://port-0-node-express2-m1gt6wu5bc1fb43d.sel4.cloudtype.app'];
-// const allowedOrigins = ['https://port-0-node-express-eu1k2lllm51c76.sel3.cloudtype.app', 'https://222leezieunpofol222.netlify.app'];
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5501'];
-const corsOptions = {
-    origin: function (origin, callback) {
-      // `origin` 값이 허용된 도메인 목록에 포함되어 있는지 확인.
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // 허용된 도메인 또는 `origin`이 `null`인 경우
-      } else {
-        callback(new Error('Not allowed by CORS')); // 허용되지 않은 도메인
-      }
-    },
-    credentials: true, // 요청에 자격 증명(Credentials)을 포함하도록 허용
-};
-app.use(cors(corsOptions));
+// const allowedOrigins = ['http://localhost:3000', 'http://localhost:5501'];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       // `origin` 값이 허용된 도메인 목록에 포함되어 있는지 확인.
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true); // 허용된 도메인 또는 `origin`이 `null`인 경우
+//       } else {
+//         callback(new Error('Not allowed by CORS')); // 허용되지 않은 도메인
+//       }
+//     },
+//     credentials: true, // 요청에 자격 증명(Credentials)을 포함하도록 허용
+// };
+// app.use(cors(corsOptions));
 
 app.set('trust proxy', 1);
 app.use(logger('dev'));
